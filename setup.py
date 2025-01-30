567a48cc3a30bb76c5d62d5507a18b01498c5e6d
Yusra Zafar <yuxraz@gmail.com>
2025-01-30 22:38:50 +0500

    requirements installed

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Yusra Zafar',
+    author_email= 'yuxraz@gmail.com',
+    description= 'End-to-end Waste Detection using YOLOv5',
+    packages= find_packages(),  # to import all the packages in the project locally
+    install_requires = []
+
+)
