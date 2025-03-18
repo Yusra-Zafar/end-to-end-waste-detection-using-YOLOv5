c17ac40c96b422f0e26836b6578e69ced13892e2
Yusra Zafar <yuxraz@gmail.com>
2025-03-18 15:46:05 +0500

    notebook experiment added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author= 'Yusra Zafar',
     author_email= 'yuxraz@gmail.com',
     description= 'End-to-end Waste Detection using YOLOv5',
-    packages= find_packages(),  # to import all the packages in the project locally
+    packages= find_packages(),  # to import all the packages in the project locally. it will look for the constructor file and will make that folder as a package.
     install_requires = []
 
 )
